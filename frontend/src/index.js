3cae06d1bac0836402cd0e8ff140ef765405c820
Ali <aifares@buffalo.edu>
2021-09-25 14:40:12 -0400

    Finished auth system

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './CSSComponents/index.css';
 import App from './Components/App.js';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
