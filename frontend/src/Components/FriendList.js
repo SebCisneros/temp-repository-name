83955344c9d591a69c0806ded3ff7c4caf418263
Jieyi <jieyili@buffalo.edu>
2021-12-02 01:48:31 -0500

    Create a dynamic friend list page #51

--- frontend/src/Components/FriendList.js
+++ frontend/src/Components/FriendList.js
@@ -1,58 +1,110 @@
-import {React, useState} from "react";
+import {React, Component} from "react";
 import { alignPropType } from "react-bootstrap/esm/DropdownMenu";
 import "../CSSComponents/FriendList.css"
 import defaultUserPic from "../media/default-user-profile-pic.svg"
-import { RiMoneyDollarBoxFill, RiChat4Fill } from "react-icons/ri";
 
-function FriendList () {
-    const [friends,setFriends] = useState([
-        {
-            username: "Floyd",
-            profilePic: defaultUserPic,
-        },
-        {
-            username: "Ali",
-            profilePic: defaultUserPic,
-        },
-        {
-            username: "Sebatian",
-            profilePic: defaultUserPic,
-        },
-        {
-            username: "seongjae",
-            profilePic: defaultUserPic,
-        },
-        {
-            username: "jieyi",
-            profilePic: defaultUserPic,
-        }
-    ]);
-
+function Friend(props) {
+    const {friend, remove, splitRequest} = props
     return (
-        <div className="body">
-            <h2>Friend List</h2>
-            {friends.length > 0 ?
-            <div className="friends_list">
-                {friends.map(frined => 
+        <div className="friend-container">
+            
+                <div className="inline"> <img className="profilePic" src={friend.profilePic} alt={friend.email} /> 
+                {friend.email}
+                </div>
+                
+                <div className="inline">
+                    <p></p>
+                </div>
+            
+            
+                <div id="push-right">
+                    <button className="button" id="remove_button" onClick={() => remove(friend.email)}>Remove Friend</button>
+                </div>
+                
+                <div  id="splitRequest">
+                    {/* modify function for this 
+                    allow this switch to request page directly and request page will show this as first name */}           
+                    <button className="button" id="request_button" onClick={() => splitRequest(friend.email)}>Request Split</button>
+                </div>  
                     
-                        <table className="friend_info">
-                            <th >
-                                <p className="userName">
-                                    <img className="profilePic" src={frined.profilePic} />
-                                    {frined.username}   
-                                </p>
-                            </th>
-                            <th>
-                                <RiMoneyDollarBoxFill className="icon" size={35}/>
-                                <RiChat4Fill className="icon" size={35} />
-                            </th>
-                            
-                        </table>
-                    )}                   
-            </div>    
-            : "Time to add new friends."}
         </div>
-    )
+    );
 }
 
-export default FriendList
+
+export default class FriendList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            friends : [],
+            input : ''
+        }
+        this.handleInput = this.handleInput.bind(this);
+        this.handleAddFriend = this.handleAddFriend.bind(this);
+        this.handleRmoveFriend = this.handleRmoveFriend.bind(this);
+        this.handleRequestSplit = this.handleRequestSplit.bind(this);
+    }
+
+    componentDidMount(){
+        this.setState({
+            friends : [
+                {email: "Jieyi@buffalo.edu", profilePic: defaultUserPic,},
+                {email: "Ali@buffalo.edu", profilePic: defaultUserPic,},
+                {email: "Sebatian@buffalo.edu", profilePic: defaultUserPic,},
+                {email: "seongjae@buffalo.edu", profilePic: defaultUserPic,},
+                {email: "Floyd@buffalo.edu", profilePic: defaultUserPic,}
+            ]
+        })
+    }
+
+    handleInput(e){
+        this.setState({
+            input : e.target.value
+        })
+    }
+
+    handleAddFriend(){
+        {this.state.input !== '' &&
+            this.setState({
+                friends: [...this.state.friends, { email: this.state.input, profilePic: defaultUserPic}],
+                input: ''
+            })
+        }
+        // this need to search if the user exist and add it to this user's friend list
+    }
+
+
+    handleRmoveFriend(email){
+        this.setState({
+            friends : this.state.friends.filter((friend) => friend.email !== email)
+        });
+
+        // this should remove this person's friend from database
+    }
+
+    handleRequestSplit(requestUser){
+        // change to the split page with the request user show as the first name 
+    }
+
+    render() {
+        return (
+            <div className="body">
+                <h2>Friend List</h2>
+                <input 
+                    type="text"
+                    placeholder="Friend Email"
+                    value={this.state.input}
+                    onChange={this.handleInput}
+                    />
+                <button id="search_button" onClick={this.handleAddFriend}>
+                Search friend
+                </button>
+                {this.state.friends.length > 0 ?
+                <div className="friends_list">
+                    {this.state.friends.map(friend => <Friend key={friend.email} friend={friend} remove={this.handleRmoveFriend} splitRequest={this.handleRequestSplit} />)}                   
+                </div>    
+                : "Time to add new friends."}
+            </div>
+        )
+    }
+}
