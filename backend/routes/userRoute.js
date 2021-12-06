f85d3b3a8bd98fb6b3092e22ba8a4ab98fdf0d10
Sebastian Cisneros <54567963+SebCisneros@users.noreply.github.com>
2021-12-06 07:36:50 -0500

    Added express routes needed for #41 & #42

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,16 +1,39 @@
-const express = require('express')
+const express = require("express");
 
 const router = express.Router();
 
-const user = require('../mongoDBmodels/user');
+const user = require("../mongoDBmodels/user");
 
-router.post('/', async (request, res) => {
-    const userInfo = new user ({
-      userName: request.body.userName
-    });
-    userInfo.save();
-
-    console.log(userInfo)
+router.post("/", async (request, res) => {
+  const userInfo = new user({
+    userName: request.body.userName,
   });
+  userInfo.save();
+
+  console.log(userInfo);
+});
+
+router.post("/addFriend", async (request, res) => {
+  doc = await user.findOne({ "userName": request.body.userName })
+  doc.friendList.push(request.body.friend)
+  doc.save()
+});
+
+router.post("/getFriendList", async (request, res) => {
+  doc = await user.find({ userName: request.body.userName });
+  res.send(doc[0].friendList);
+});
+
+router.get("/getAllUsers", async (request, res) => {
+  user.find({}, function (err, users) {
+    var userMap = [];
+
+    users.forEach(function (user) {
+      userMap.push(user.userName);
+    });
+
+    res.send(userMap);
+  });
+});
 
 module.exports = router;
