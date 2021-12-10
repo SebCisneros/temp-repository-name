fb5f812c8601067e258952057daee50b2016e9eb
Jieyi <jieyili@buffalo.edu>
2021-12-09 23:55:31 -0500

    Convert request split form from react component to react hook #57

--- frontend/src/Components/RequestSplitForm.js
+++ frontend/src/Components/RequestSplitForm.js
@@ -1,178 +1,147 @@
-// Reference: https://bapunawarsaddam.medium.com/add-and-remove-form-fields-dynamically-using-react-and-react-hooks-3b033c3c0bf5
-import React, { Component } from 'react'
+import { useState,useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../CSSComponents/RequestSplit.css"
+import { useAuth } from "../context/AuthContext";
+import { getFriendList } from "../axiosFunctions"
 
+export default function Split() {
+    const [indexOfItem, setIndexOfItem] = useState(0);
+    const currentUser = useAuth()
+    const [requestSection, setRequestSection] = useState([]);
+    const [friends, setFriends] = useState([]);
 
-export default class Split extends Component {
-   
-    constructor(props) {
-        super(props);
-        this.state = {
-            requestSection: [{ username: "", itemPrice: [0]}],
-            Friends: []                            // username has to be unique
-            
+    useEffect(() => {
+        async function setUp (){
+            // get all the friend of current user
+            var friends_helper = ["Ali@gmail.com", "Seb@gmail.com","check@gmali.com"];
+            //var friends_helper = await getFriendList(currentUser.email)
+            setFriends(friends_helper);
+
+            // change the requestSection
+            var requestSection_helper = [{friend: "", itemPrice: [0], total: 0}];
+            if (friends_helper.length > 0){
+                requestSection_helper = [{friend: friends_helper[0], itemPrice: [0], total: 0}]
+            }
+            setRequestSection(requestSection_helper);
         }
-        this.handleChangeUsername = this.handleChangeUsername.bind(this);
-        this.handleChangeItemPrice = this.handleChangeItemPrice.bind(this);
-        this.addItem = this.addItem.bind(this);
-        this.removeRequestSection = this.removeRequestSection.bind(this);
-        this.addRequestSection = this.addRequestSection.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        setUp()
+    },[])
+
+
+    function handleChangeFriend(setionIndex, email){
+        let requestSectionHelp = requestSection;
+        requestSectionHelp[setionIndex].Friend = email;
+        setRequestSection(requestSectionHelp);
     }
 
-    // need to be updated
-    getUserFriends() {
-        return ["Ali@gmail.com", "Seb@gmail.com","check@gmali.com"];
+    function handleChangeItemPrice(setionIndex, itemIndex, itemPrice){
+        let requestSectionHelp = requestSection;
+        requestSectionHelp[setionIndex].itemPrice[itemIndex] = itemPrice;
+        var total = 0;
+        requestSectionHelp[setionIndex].total = total;
+        setRequestSection(requestSectionHelp);
     }
 
-    componentDidMount(){
-        this.setState({
-            Friends: this.getUserFriends()}); 
-
-        //const response = await fetch('https://api.npms.io/v2/search?q=react')                              // unsure about the link
-        //const data = await response.json();
-        //this.setState({ Friends: data.Friends }));
+    function addItem(setionIndex){
+        document.write(requestSection.length)
+        let requestSectionHelp = requestSection;
+        requestSectionHelp[setionIndex].itemPrice = [...requestSection[setionIndex].itemPrice, 0];
+        setRequestSection(requestSectionHelp);
     }
 
-    handleChangeUsername(i, e) {
-        let requestSectionHelp = this.state.requestSection;
-        requestSectionHelp[i].username = e.target.value;
-        this.setState({
-            requestSection: requestSectionHelp});
+    function removeRequestSection(index){
+        let requestSectionHelp = requestSection;
+        requestSectionHelp.splice(index, 1)
+        setRequestSection(requestSectionHelp);
     }
 
-    handleChangeItemPrice(setionIndex, itemIndex, e) {
-        let requestSectionHelp = this.state.requestSection;
-        requestSectionHelp[setionIndex].itemPrice[itemIndex] = e.target.value;
-        this.setState({
-            requestSection: requestSectionHelp
-        });
-    }
-
-    addItem(setionIndex){
-        let requestSectionHelp = this.state.requestSection;
-        requestSectionHelp[setionIndex].itemPrice = [...this.state.requestSection[setionIndex].itemPrice, 0]
-        this.setState({
-            requestSection: requestSectionHelp
-        });
-    }
-
-    removeRequestSection(i,e){
-        let requestSectionHelp = this.state.requestSection;
-        requestSectionHelp.splice(i, 1);
-        this.setState({ 
-            requestSection: requestSectionHelp
-         });
-    }
-
-    addRequestSection(event){
-        this.setState({
-            requestSection: [...this.state.requestSection, { username: "", itemPrice :  [0]}]
-          })
-    }
-
-    handleSubmit(event){
-        event.preventDefault();
-        const result = {
-            requestSection: this.state.requestSection
-        }
-        console.log(result)                                             // send post request and use that number in Calculate
-        
-        //fetch('https://mywebsite.com/endpoint/', {
-        //    method: 'POST',
-        //    headers: {
-        //        'Accept': 'application/json',
-        //        'Content-Type': 'application/json',
-        //    },
-        //    body: JSON.stringify({
-        //        requestSection: this.state.requestSection
-        //      })
-        //})
-        
+    function addRequestSection(){
+        setRequestSection([...requestSection, {username: friends[0], itemPrice :  [0], total: 0}])
     }
 
 
-    render() {
-        return (
-            <div id="center">
-                <div id="instruction">
-                    <h6>Please Fill out the form.
-                        <br/>Use Add-Item button when you have more items request from the user
-                        <br />Use Add-Friend button when you have more friend you want to request split from.
-                    </h6>
-                </div>
-                <form onSubmit={this.handleSubmit}>
-                    {/* loop through requestSection */}
-                    {this.state.requestSection.map((element, index) => (
-                        <div className='one-user-request' key={(index).toString()}>
+    async function handleSubmit(){
+        document.write(friends)
+    }
 
-                            {/* username */}
-                            <label key={(index+1).toString()}>Username: </label>
-                            <select 
-                                required
-                                key={(index+2).toString()}
-                                value={element.username}
-                                onChange={e => this.handleChangeUsername(index, e)}
-                                >
-                                {/* loops through this.state.Friends */}
-                                {
-                                    this.state.Friends.map((user) =>
-                                        <option
-                                            key={user}
-                                            value={user}
-                                            >{user}
-                                        </option>
-                                    )
-                                }
-                            </select>
-
-                            {/* item price input list */}
-                            {/* loop through the item price to insert add more items */}
-                            {element.itemPrice.map( (item, i) =>
-                                <div className="item-list">
-                                    <label key={(index+5+i).toString()}>Item Price: </label>
-                                    <input
-                                        required
-                                        key={(index+6+i).toString()}
-                                        type='number'
-                                        min='0'
-                                        step='0.01'
-                                        value={item || 0}
-                                        onChange={e => this.handleChangeItemPrice(index, i, e)} />
-                                </div>
-                            )}
-
-                            <div className="button-section">
-                                {/* press a add item button for adding more item input for the request section */}
-                                <button key={(index+3).toString()} className="button" id="addItem" type="button" onClick={() => this.addItem(index) }>Add Item</button>
-                                
-                                {/* press a remove button for each requestsection after the first one */}
-                                {
-                                    index ?
-                                        <button 
-                                            key={(index+4).toString()}
-                                            type="button"  
-                                            className="button" 
-                                            id="remove" 
-                                            onClick={() => this.removeRequestSection(index)}>Remove Freind</button>
-                                            : null
-                                }
-                            </div>
-                            <br />
-                            <br />
-                            
-                        </div>
-                        ))}
-
-                    <br />
-                    <div className="button-section">
-                        <button className="button" id="addFriend" type="button" onClick={() => this.addRequestSection()}>Add Friend</button>
-                        <button className="button" id="Submit" type="submit"><Link to="/profile" style={{ color: 'inherit', textDecoration: 'inherit'}}>Submit</Link></button>
-                    </div>
-                </form>
-                
+    return (
+        <div id="center">
+            <div id="instruction">
+                <h6>Please Fill out the form.
+                    <br/>Use Add-Item button when you have more items request from the user
+                    <br />Use Add-Friend button when you have more friend you want to request split from.
+                </h6>
             </div>
-        )
-    }
-}
+
+            <form onSubmit={handleSubmit}>
+                {/* loop through requestSection */}
+                {requestSection.map((element, index) => (
+                    <div className='one-user-request' key={index}>
+
+                        {/* username */}
+                        <label key={index+1}>Username: </label>
+                        <select 
+                            required
+                            key={index+2}
+                            value={element.username}
+                            onChange={e => handleChangeFriend(index, e.target.value)}
+                        >
+                            {/* show a list of current user's friends */}
+                            {
+                                friends.map((user) =>
+                                    <option
+                                        key={user}
+                                        value={user}
+                                        >{user}
+                                    </option>
+                                )
+                            }
+                        </select>
+
+                        {/* item price input list */}
+                        {element.itemPrice.map( (item, i) =>
+                            <div className="item-list">
+                                <label key={(index+5+i).toString()}>Item Price: </label>
+                                <input
+                                    required
+                                    key={(index+6+i).toString()}
+                                    type='number'
+                                    min='0'
+                                    step='0.01'
+                                    value={item || 0}
+                                    onChange={e => handleChangeItemPrice(index, i, e.target.value)} />
+                            </div>
+                        )}
+
+                        <div className="button-section">
+                            {/* press a add item button for adding more item input for the request section */}
+                            <button key={index+3} className="button" id="addItem" type="button" onClick={() => addItem(index) }>Add Item</button>
+                            
+                            {/* press a remove button for each requestsection after the first one */}
+                            {
+                                index ?
+                                    <button 
+                                        key={index+4}
+                                        type="button"  
+                                        className="button" 
+                                        id="remove" 
+                                        onClick={() => removeRequestSection(index)}>Remove Freind</button>
+                                        : null
+                            }
+                        </div>
+                        <br />
+                        <br />
+                        
+                    </div>
+                    ))}
+
+                <br />
+                <div className="button-section">
+                    <button className="button" id="addFriend" type="button" onClick={() => addRequestSection()}>Add Friend</button>
+                    <button className="button" id="Submit" type="submit"><Link to="/profile" style={{ color: 'inherit', textDecoration: 'inherit'}}>Submit</Link></button>
+                </div>
+            </form>
+            
+        </div>
+    )
+}                                    
